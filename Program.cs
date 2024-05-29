a15460186aa727a0541ddf14944d2234b12d3cfb
Ismail <126619125+is9700@users.noreply.github.com>
2024-05-29 19:15:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,15 +6,16 @@ class Program
 {
     static async Task Main()
     {
-        int numTasks = 10;
+        int numTasks = 10;  // Anzahl der Tasks 
 
-        var tasks = Enumerable.Range(1, numTasks)
+        var tasks = Enumerable.Range(1, numTasks)        // Eine Sequenz von Tasks erstellen, die eine Verzogerung hat(100ms)
+
                                .Select(taskId => MyCallableAsync(taskId))
                                .ToArray();
 
-        await Task.WhenAll(tasks);
+        await Task.WhenAll(tasks);  // auf  Alle Tasks warten, bis sie abgeschlossen sind
     }
-
+    // Asynchrone Methode, die einen Task mit einer Verzogerung erstel
     static async Task MyCallableAsync(int taskId)
     {
         Console.WriteLine($"Task {taskId} started by thread: {Environment.CurrentManagedThreadId}");
@@ -41,7 +42,7 @@ class Program
         for (int i = 0; i < numTasks; i++)
         {
             int taskId = i + 1;
-            tasks[i] = MyCallableAsync(taskId);
+            tasks[i] = MyCallableAsync(taskId); // Eine asynchrone Methode aufrufen und den resultierenden Task dem Array hinzufugen
         }
 
         await Task.WhenAll(tasks);
